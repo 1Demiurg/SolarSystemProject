78108a268176fa4b7e307dfff75fa2df91f1b9ae
student1 <petrov.alexander.00@bk.ru>
2017-03-05 16:04:08 +0300

    initial

--- app/src/main/java/itschool/samsung/ru/space/framework/GameView.java
+++ app/src/main/java/itschool/samsung/ru/space/framework/GameView.java
@@ -15,23 +15,29 @@ import java.util.ArrayList;
 public class GameView extends View {
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        for (int i = 0; i < 50; i++)
-        {
-            objects.add(new TestObj());
-        }
+        float d=25;
+        objects.add(new TestObj("Солнце",139.1f/2,0,"RED",0,0));
+        objects.add(new TestObj("Меркурий",0.24f*2,3+1.48f,"GREEN",1,d*3));
+        objects.add(new TestObj("Венера",0.6f*2,3+1.5f,"YELLOW",2,d*1.5f));
+        objects.add(new TestObj("Земля",0.63f*2+1,2.97f,"GREEN",3,d*1.25f));
+        objects.add(new TestObj("Марс",0.33f*2+1,2.41f,"RED",4,d));
+        objects.add(new TestObj("Юпитер",6.99f*2+1,1.3f,"GREEN",5,d));
+        objects.add(new TestObj("Сатурн",5.82f*2+1,0.96f,"YELLOW",6,d));
+        objects.add(new TestObj("Уран",2.53f*2+1,0.68f,"BLUE",7,d));
+        objects.add(new TestObj("Нептун",2.46f*2+1,0.54f,"BLUE",8,d));
     }
     ArrayList<Object> objects = new ArrayList<>();
 
-    float lastUpdate = System.currentTimeMillis();
+    long lastUpdate = System.currentTimeMillis();
 
     @Override
+
     protected void onDraw(Canvas canvas){
         long currentTime = System.currentTimeMillis();
         drawObjects(canvas);
         updateObjects((currentTime - lastUpdate)*0.01f);
         lastUpdate = currentTime;
         invalidate();
-
     }
     void drawObjects(Canvas canvas)
     {
@@ -48,6 +54,7 @@ public class GameView extends View {
         for (int i = 0; i< objects.size(); i++){
             if (objects.get(i) instanceof Updatable)
             {
+
                 ((Updatable)objects.get(i)).update(dt);
             }
 
--- app/src/main/java/itschool/samsung/ru/space/framework/TestObj.java
+++ app/src/main/java/itschool/samsung/ru/space/framework/TestObj.java
@@ -3,6 +3,11 @@ package itschool.samsung.ru.space.framework;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.annotation.Dimension;
+import android.view.Window;
+
+import itschool.samsung.ru.space.framework.Renderable;
+import itschool.samsung.ru.space.framework.Updatable;
 
 /**
  * Created by vv on 14.11.2016.
@@ -10,28 +15,45 @@ import android.graphics.Paint;
 public class TestObj implements Renderable, Updatable {
     float x;
     float y;
-    float vx;
-    float vy;
+    float ox;
+    float py;
+    float t;
+    float g;
+    float move;
+    float location;
+    float between;
+    String name;
+    String color;
 
-    public TestObj() {
-        paint.setColor(Color.GREEN);
-        x = 300;
-        y = 300;
-        vx = (float) (Math.random() * 10 - 5);
-        vy = (float) (Math.random() * 10 - 5);
+
+    public TestObj(String name,float g,float move, String color,float location,float between) {
+        this.name=name;
+        this.g=g;
+        this.move=move;
+        this.color = color;
+        paint.setColor(Color.parseColor(color.toLowerCase()));
+        this.location=location;
+        this.between=between;
+        x = 225;
+        y = 275;
     }
+
+
 
     Paint paint = new Paint();
 
     @Override
     public void render(Canvas canvas) {
-        canvas.drawRect(x, y, x + 200, y + 200, paint);
+
+        canvas.drawCircle(ox, py,g, paint);
 
     }
 
     @Override
     public void update(float dt) {
-        x += dt * vx;
-        y += dt * vy;
+            t +=move/100;
+            ox = between* location  * (float) Math.cos((double) t) + x;
+            py = between* location *(float) Math.sin((double) t) + y;
+
     }
 }
